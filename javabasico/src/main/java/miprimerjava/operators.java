4d4e73fb6455f2a2dd45118534d777bf0ae3b0e1
MANUELA\manue <manuelaplanelles@gmail.com>
2026-01-07 00:20:03 +0100

    ejercicio de operadores

---
+++ javabasico/src/main/java/miprimerjava/operators.java
@@ -0,0 +1,15 @@
+package miprimerjava;
+
+public class operators {
+    public static void main(String[] args) {
+        int a= 20, b = 10, c = 0, d = 20, e = 40, f = 30;
+        String x = "Thanks", y = "You";
+
+        System.out.println(("a+b= " +(a+b)));
+        System.out.println(("a-b= " +(a-b)));
+        System.out.println(("x+y= " +(x+y)));
+        System.out.println(("a*b= " +(a*b)));
+        System.out.println(("a/b= " +(a/b)));
+        System.out.println(("a%b= " +(a%b)));
+    }
+}

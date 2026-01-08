a676b7dec4b859aa00eb9e500a5b91246fed42f6
A613\1DAW <manuelaplanelles@gmail.com>
2026-01-08 17:23:34 +0100

    proporcina mas informacion a la explicación

--- javabasico/src/main/java/UD4/Repositividad.java
+++ javabasico/src/main/java/UD4/Repositividad.java
@@ -6,12 +6,20 @@ public class Repositividad {
 
     static void main(){
         imprimir(1);
+        System.out.println(suma(10));
     }
     public static void imprimir(int num){
         if (num<=limite){
             System.out.println(num);
             imprimir(num+1);
             return;
+        }
+    }
+    public static int suma (int nums){
+        if (nums == 0) {
+            return 0;
+        }else{
+            return nums + suma(nums-1);
         }
     }
 }

25cf9d2919b88f59c91813c8f145b18168265d97
MANUELA\manue <manuelaplanelles@gmail.com>
2026-02-04 23:50:18 +0100

    editamos la repeticion de curso y hora

--- javabasico/src/main/java/practicas/LombokInstituto/Curso.java
+++ javabasico/src/main/java/practicas/LombokInstituto/Curso.java
@@ -9,4 +9,17 @@ public class Curso {
     @NonNull
     private String nombre;
     private int horas;
+
+    @Override
+    public boolean equals(Object otroCurso) {
+        if (this == otroCurso) return true;
+        if (otroCurso == null || getClass() != otroCurso.getClass()) return false;
+        Curso curso = (Curso) otroCurso;
+        return horas == curso.horas && nombre.equals(curso.nombre);
+    }
+
+    @Override
+    public int hashCode() {
+        return java.util.Objects.hash(nombre, horas);
+    }
 }
--- javabasico/src/main/java/practicas/LombokInstituto/Instituto.java
+++ javabasico/src/main/java/practicas/LombokInstituto/Instituto.java
@@ -40,5 +40,4 @@ public class Instituto {
             System.out.println("El curso ya existe en el instituto.");
         }
     }
-
 }

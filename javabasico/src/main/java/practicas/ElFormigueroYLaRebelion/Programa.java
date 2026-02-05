49ec381e824e7d419d2ff837205d7ab8270285a6
Manuela <manuelaplanelles@gmail.com>
2026-02-05 17:18:52 +0100

    clase

--- javabasico/src/main/java/practicas/ElFormigueroYLaRebelion/Programa.java
+++ javabasico/src/main/java/practicas/ElFormigueroYLaRebelion/Programa.java
@@ -1,4 +1,20 @@
 package practicas.ElFormigueroYLaRebelion;
 
+import java.util.ArrayList;
+
 public class Programa {
+    private String nombre;
+    private Cadena cadena;
+    private int temporadas;
+
+    private ArrayList<Empleado>  listaEmpleado = new ArrayList<>();
+    private ArrayList<Invitado> listaInvitado = new ArrayList<>();
+
+    public Programa(String nombre, Cadena cadna, int temporadas, ArrayList listaEmpleado, ArrayList listaInvitado){
+        this.nombre = nombre;
+        this.cadena = cadena;
+        this.temporadas = 0;
+        this.listaInvitado=listaInvitado;
+        this.listaEmpleado=listaEmpleado;
+    }
 }

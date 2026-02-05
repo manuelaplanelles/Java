e4067452db45e21ed6536b7e3efae6f4ae1d6bd3
Manuela <manuelaplanelles@gmail.com>
2026-02-05 16:42:36 +0100

    m

--- javabasico/src/main/java/practicas/ElFormigueroYLaRebelion/Cadena.java
+++ javabasico/src/main/java/practicas/ElFormigueroYLaRebelion/Cadena.java
@@ -1,4 +1,8 @@
 package practicas.ElFormigueroYLaRebelion;
 
+import java.util.ArrayList;
+
 public class Cadena {
+    private String nombre;
+    ArrayList<Programa> = new ArrayList<listaPrograma>;
 }

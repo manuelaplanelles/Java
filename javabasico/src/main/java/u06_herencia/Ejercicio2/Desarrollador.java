012bf23fa975550cec5111a9e7c9cd67e5219196
Manuela <manuelaplanelles@gmail.com>
2026-02-10 21:12:54 +0100

    ejercicio2

---
+++ javabasico/src/main/java/u06_herencia/Ejercicio2/Desarrollador.java
@@ -0,0 +1,10 @@
+package u06_herencia.Ejercicio2;
+
+public class Desarrollador extends Empleados {
+
+    @Override
+    public void realizarTarea(){
+        System.out.println("Escribiendo código y solucionando bugs.");
+
+    }
+}
---
+++ javabasico/src/main/java/u06_herencia/Ejercicio2/Empleados.java
@@ -0,0 +1,7 @@
+package u06_herencia.Ejercicio2;
+
+public class Empleados {
+    public void realizarTarea(){
+        System.out.println("Empleado realizando una tarea genérica.");
+    }
+}

033f70a3569346b13d1738af0e69d55dff9aac21
Manuela <manuelaplanelles@gmail.com>
2026-02-23 18:59:23 +0100

    eliminadas diapositivas de js

--- Diapositiva39.js
+++
@@ -1,6 +0,0 @@
-//Actividad 1: función que devuelve el doble.
-function doble(numero) {
-    return numero * 2;
-}
-let resultado = doble(5);
-console.log(resultado); // 10
--- Diapositiva41.js
+++
@@ -1,7 +0,0 @@
-//Actividad 2: Número primo.
-const areaTriangulo = (base, altura) => {
-    return (base * altura) / 2;
-};
-// Forma aún más corta (return implícito):
-const areaTriangulo = (base, altura) => (base * altura) / 2;
-console.log(areaTriangulo(10, 5)); // 25

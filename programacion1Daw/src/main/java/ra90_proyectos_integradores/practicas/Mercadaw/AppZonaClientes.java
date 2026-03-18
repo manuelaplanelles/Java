5b1660459f2a5a53288bf30d565889441376c318
MANUELA\manue <manuelaplanelles@gmail.com>
2026-03-18 16:25:48 +0100

    m

--- programacion1Daw/src/main/java/ra90_proyectos_integradores/practicas/Mercadaw/AppZonaClientes.java
+++ programacion1Daw/src/main/java/ra90_proyectos_integradores/practicas/Mercadaw/AppZonaClientes.java
@@ -1,4 +1,21 @@
 package ra90_proyectos_integradores.practicas.Mercadaw;
 
 public class AppZonaClientes {
+
+
+    public class AppZonaCliente(){
+
+    }
+    public class void autentificacion(){
+
+    }
+    public class void iniciarCompra(){
+
+    }
+    public class void imprimirProductos(){
+
+    }
+    public class void imprimirDespedida(){
+        
+    }
 }
--- programacion1Daw/src/main/java/ra90_proyectos_integradores/practicas/Mercadaw/Producto.java
+++ programacion1Daw/src/main/java/ra90_proyectos_integradores/practicas/Mercadaw/Producto.java
@@ -1,4 +1,8 @@
 package ra90_proyectos_integradores.practicas.Mercadaw;
 
 public enum Producto {
+    MANZANAS(2.30), PAN(1.00), ARROZ(3.50), POLLO(4.30), LECHE(1.30), ACEITE(8.30), HUEVOS(3.30), TOMATES(4.00), PASTA(0.39);
+
+    Producto(double precio) {
+    }
 }
